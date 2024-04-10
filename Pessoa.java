1fb194cebd0f77bef9d4755679dd8d5af7accc72
EduardaChiarelli <166647190+EduardaChiarelli@users.noreply.github.com>
2024-04-10 17:15:59 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,37 +1,39 @@
 package listaUm;
 
 public class Pessoa {
+   private String nome; 
+   private int idade;
+   private double altura; 
 
-	private String nome; 
-	private int idade;
-	private double altura; 
-	public Pessoa (String n, int i, double a) 
-	{
+	
+ public Pessoa (String n, int i, double a) 
+{
 	this.nome = n; 
 	this.idade = i;
 	this.altura = a;
+ }
+ public String getNome() {
+     return nome;
 	}
-	public String getNome() {
-	   return nome;
+	
+ public int getIdade() {
+     return idade;
 	}
-	public int getIdade() {
-	return idade;
+ public double getAltura() {
+     return altura;
 	}
-	public double getAltura() {
-	return altura;
+ public void setNome(String n) {
+     this.nome = n; 
 	}
-	public void setNome(String n) {
-	this.nome = n; 
+public void setIdade(int i) {
+     this.idade = i; 
 	}
-	public void setIdade(int i) {
-	this.idade = i; 
+public void setAltura(double a) {
+     this.altura = a; 
 	}
-	public void setAltura(double a) {
-	this.altura = a; 
-	}
-	public String toString() {
-	return "Nome: " + nome +
-	"\nIdade: " + idade + "\nAltura: " + altura;
+	
+public String toString() {
+      return "Nome: " + nome + "\nIdade: " + idade + "\nAltura: " + altura;
 	}
 	
 }
